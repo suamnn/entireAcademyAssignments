2fff1fefedc10dafbaf1c5e622ffaacd864e03a2
anishniroula <anishniroula1@gmail.com>
2023-03-09 16:08:18 -0500

    initial commit

---
+++ src/main/java/com/entireAcademy/Day2/dataTypes/Day2Note.java
@@ -0,0 +1,48 @@
+package com.entireAcademy.Day2.dataTypes;
+
+public class Day2Note {
+    public static void main(String[] args) {
+//        byte num = 20;
+//        int wholeNumber = 200000;
+//        double decimal = 1.2323232322323232323;
+//        float fDecimal = 1.34f;
+//        char c = 'A';
+//        short shrt = 12121;
+//        long lng = 232537523752372537L;
+//        boolean isTall = false;
+
+        // comment
+        double a =  100.12;
+        int b = 21;
+//        // 21 * 4 = 84
+//        // 100 - 84 = 16;
+//        int add = a + b;
+//        int subtract = a - b;
+//        int multiplication = a * b;
+//        int division = a / b;
+//        int modulo = a % b;
+
+//        int findNearestNumber = (int) (a / b);
+
+        String message = "Hello, world!";
+
+//        String firstName = "John";
+//        String lastName = "Doe";
+//        String fullName = firstName + " " + lastName;
+
+        // Retrieving the length of a string
+//        String word = "hello";
+//        int length = word.length(); // length = 5
+//
+//
+
+        // Checking if a string contains a certain substring
+//        boolean contains = message.contains("world"); // contains = true
+
+//        String replaced = message.replace("world", "Java");
+//        String sentence = "The quick brown fox";
+//        String[] words = sentence.split(" "); // words = ["The", "quick", "brown", "fox"]
+
+//        System.out.println(words);
+    }
+}
---
+++ src/main/java/com/entireAcademy/Greeter.java
@@ -0,0 +1,7 @@
+package com.entireAcademy;
+
+public class Greeter {
+	public String sayHello() {
+		return "Hello world!";
+	}
+}
