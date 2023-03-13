47d4df296703c195b6e358fc66f5c67992006d92
anishniroula <anishniroula1@gmail.com>
2023-03-13 11:24:35 -0400

    Day 3

---
+++ src/main/java/com/entireAcademy/Day3/expression/Day3ExpressionClassNote.java
@@ -0,0 +1,67 @@
+package com.entireAcademy.Day3.expression;
+
+public class Day3ExpressionClassNote {
+    public static void main(String[] args) {
+//        int a = 10;
+//        int b = 9;
+//
+//        boolean isSame = a <= b;
+//        System.out.println(isSame);
+
+//        int x = 5;
+//        int y = 6;
+//        if (x > 20 || y > 10) {
+//            System.out.println("x is positive and y is less than 10");
+//        } else {
+//            System.out.println("false statement");
+//        }
+
+//        String sentence = "My Name is Bishal";
+//
+//        if (sentence.contains("Bishal")) {
+//            System.out.println("My Name is Bishal");
+//        } else if(sentence.contains("Name")) {
+//            System.out.println("Name exist");
+//        } else {
+//            System.out.println("My Name is not Bishal");
+//        }
+
+//        int dayOfWeek = 2;
+//        switch (dayOfWeek) {
+//            case 1:
+//                System.out.println("Monday");
+//                break;
+//            case 2:
+//                System.out.println("Tuesday");
+//                break;
+//            case 3:
+//                System.out.println("Wednesday");
+//                break;
+//            default:
+//                System.out.println("Other day");
+//        }
+
+//        String name = "jpg";
+//
+//        switch (name) {
+//            case "Bishal":
+//                System.out.println("I am not Ashish, I am Bishal");
+//            case "Ashish":
+//                System.out.println("I am Ashish");
+//                break;
+//            default:
+//                System.out.println("No name found");
+//        }
+
+//        int myInteger = 2;
+//        // T      &&            F       ||       F       ||        F          ||       F
+//        if(myInteger==2 && myInteger==3 || myInteger==4 || myInteger%2==0 || myInteger==6) {
+//            System.out.println("the combined statement is true.");
+//        }
+//        else {
+//            System.out.println("the combined statement is false.");
+//        }
+
+    }
+
+}
