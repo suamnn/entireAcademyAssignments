776000070f0f47c392bbee2d8b550cf4f9e22625
anishniroula <anishniroula1@gmail.com>
2023-03-13 21:39:29 -0400

    day 4

---
+++ src/main/java/com/entireAcademy/Day4/ArraysLoop/ArrayAndLoopsClassNote.java
@@ -0,0 +1,92 @@
+package com.entireAcademy.Day4.ArraysLoop;
+
+import java.util.Arrays;
+
+public class ArrayAndLoopsClassNote {
+    public static void main(String[] args) {
+        int[] numbers = new int[5];
+        numbers[0] = 1;
+        numbers[1] = 2;
+        numbers[2] = 3;
+        numbers[3] = 4;
+        numbers[4] = 5;
+//        System.out.println(numbers[1]);
+        numbers[1] = 10;
+
+//        System.out.println(numbers[1]);
+
+//        int[] myArray = {1,2,4,5,6};
+//
+//        System.out.println(Arrays.toString(myArray));
+
+//        int[] myArray = {1, 2, 3, 4, 5};
+//        for (int i = 0; i < myArray.length; i++) {
+//            System.out.println(myArray[i]);
+//        }
+
+//        int[][] multiDim = {{123,123,124}, {000,1212, 121212}};
+
+//        int[] myArray = {5, 3, 1, 2, 4};
+//        Arrays.sort(myArray);
+//        System.out.println(Arrays.toString(myArray)); // Output: [1, 2, 3, 4, 5]
+
+//        String[] myArray = {"JavaFX", "HBase", "OpenCV", "Java", "Hadoop","Neo4j"};
+//        Arrays.sort(myArray);
+//        System.out.println(Arrays.toString(myArray));
+
+//        for (int i = 1; i <= 10; i++) {
+//            System.out.println(i);
+//        }
+
+//        int sum = 10;
+//        for (int i = 0; i <= 10; i++) {
+//            sum += i;
+//            System.out.println(sum);
+//        }
+////        System.out.println(sum);
+//    }
+
+//        for (int i = 10; i >= 1; i--) {
+//            System.out.println(i);
+//        }
+
+//        int i = 1;
+//        while(i <= 20) {
+//            if (i % 2 == 0) {
+////                System.out.println(i);
+////            }
+////            if (i == 7) {
+////                break;
+////            }
+////            i++;
+////            // code block
+////            // increment/decrement
+//        }
+//        int i = 5;
+//        while (i <= 4) {
+//            System.out.println(i + "While loop");
+//            i++;
+//        }
+//
+//        do {
+//            System.out.println(i + " do-while loop");
+//            i++;
+//        } while (i <= 4);
+
+//        String[] myArray = {"JavaFX", "HBase", "OpenCV", "Java", "Hadoop","Neo4j"};
+//        int arrayLength = myArray.length;
+//        for (int i = 0; i <= arrayLength - 1; i++) {
+//            for (int j = i + 1; j < arrayLength; j++) {
+//                if (myArray[i].compareTo(myArray[j]) > 0) {
+//                    String temp = myArray[i];
+//                    myArray[i] = myArray[j];
+//                    myArray[j] = temp;
+//                }
+//            }
+//        }
+//
+//        System.out.println(Arrays.toString(myArray));
+
+    }
+
+    }
